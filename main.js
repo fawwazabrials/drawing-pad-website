68f103024be024aaac2b189028714eba80831b90
fawwazabrials <fawwazabrial.fas@gmail.com>
2023-03-22 17:43:01 +0700

    Change doube quotes to single quotes

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const DEFAULT_SIZE = 16;
 
-const grid = document.querySelector(".container");
+const grid = document.querySelector('.container');
 
 function drawGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
     for (let i=0; i<size*size; i++) {
-        const gridItem = document.createElement("div");
-        gridItem.className = "grid-item";
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
         grid.appendChild(gridItem);
     }
 }
