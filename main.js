2978ce768de8a41d2271b6e7b98f0627fa78a0d6
fawwazabrials <fawwazabrial.fas@gmail.com>
2023-03-22 17:41:24 +0700

    Draw sketch grid on page

---
+++ main.js
@@ -0,0 +1,15 @@
+const DEFAULT_SIZE = 16;
+
+const grid = document.querySelector(".container");
+
+function drawGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+    for (let i=0; i<size*size; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.className = "grid-item";
+        grid.appendChild(gridItem);
+    }
+}
+
+drawGrid(DEFAULT_SIZE);
