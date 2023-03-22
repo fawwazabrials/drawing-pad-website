ffb3851a5b478afdb3b8b79598a37ad7bdf85091
fawwazabrials <fawwazabrial.fas@gmail.com>
2023-03-22 22:07:07 +0700

    Create hover effect for drawing

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const DEFAULT_SIZE = 16;
+let clicked = false;
 
 const grid = document.querySelector('.container');
 
@@ -8,6 +9,7 @@ function drawGrid(size) {
     for (let i=0; i<size*size; i++) {
         const gridItem = document.createElement('div');
         gridItem.className = 'grid-item';
+        gridItem.ondragstart = () => false;
         grid.appendChild(gridItem);
     }
 }
@@ -16,4 +18,23 @@ function clearGrid() {
     grid.innerHTML = '';
 }
 
+function fillGrid(el) {
+    if (clicked) {
+        el.target.style.backgroundColor = 'black';
+    }
+}
+
+function listen() {
+    // create event listener for click / no click
+    document.querySelector('body').addEventListener('mousedown', (e) => clicked = true);
+    document.querySelector('body').addEventListener('mouseup', (e) => clicked = false);
+
+    // create event listeners for grid pixels
+    gridItems = document.querySelectorAll('.grid-item');
+    for (let i=0; i<gridItems.length; i++) {
+        gridItems[i].addEventListener('mouseover', fillGrid);
+    }
+}
+
 drawGrid(DEFAULT_SIZE);
+listen();
